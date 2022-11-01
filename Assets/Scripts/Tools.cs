f5111aca9b8a5ba663aaca719378ecc1a97d0384
SimonDeb <simon.deb62@gmail.com>
2022-11-01 11:43:36 +0100

    Détection Collider Fox <=> objet   +   Amélioration Collider Area Scene 1 et 2

---
+++ Assets/Scripts/Tools.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tools : MonoBehaviour
+{
+    public static int GetRandomNumber(int minNumber, int MaxNumber)
+    {
+        int result = 0;
+
+        return result;
+    }
+}
