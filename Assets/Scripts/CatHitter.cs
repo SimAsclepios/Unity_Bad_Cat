b4c6703fc0bfbd3d5d17907e6a8ba8649ffdc8d3
SimonDeb <simon.deb62@gmail.com>
2022-11-02 21:10:45 +0100

    Comptage Score + effet Score gagné au dessus du perso

--- Assets/Scripts/CatHitter.cs
+++ Assets/Scripts/CatHitter.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CatHitter : MonoBehaviour
 {
     public float f_explosionForce;
+    public GameObject ScoreAdd;
+    public GameObject TextScore;
     /// <summary>
     /// Liste de particules a générer quand une nouvelle collision est détectée
     /// </summary>
     public GameObject[] hitParticles;
+
     protected string[] hitVerbs = new string[]
     {
         "piétine", "détruit", "frappe", "a cassé", "fonce sur", "explose", "fait voler"
@@ -20,23 +24,35 @@ public class CatHitter : MonoBehaviour
     protected readonly System.Random random = new System.Random();
 
     // Méthode Unity !!!  Comme les méthodes Start et Update en fait ! ...
-    private void OnControllerColliderHit(ControllerColliderHit hit)
+    // Entre dans la méthode quand une collision du GameObject dans lequel on a mis ce script est detecté avec un autre GameObject (capté en paramètre de méthode)
+    private void OnControllerColliderHit(ControllerColliderHit obj_Collider)
     {
-        Rigidbody obj_rb = hit.gameObject.GetComponent<Rigidbody>();
-        if (hit.gameObject.CompareTag("Hittable") && obj_rb.mass <= 10 && !hit.gameObject.name.Contains("Plane") && !hit.gameObject.name.Contains("Wall"))
+        Rigidbody objCol_RB = obj_Collider.gameObject.GetComponent<Rigidbody>();
+        if (obj_Collider.gameObject.CompareTag("Hittable") && objCol_RB.mass <= 10 && !obj_Collider.gameObject.name.Contains("Plane") && !obj_Collider.gameObject.name.Contains("Wall"))
         {
             // Max value - 1 pour la vrai valeur maximale possible ..
-            print("Le renard " + hitVerbs[random.Next(0, 7)] + " " + hit.gameObject.name);
-            // Change le Tag du GameObject pour ne gagner qu'une fois les points à sa collision
-            hit.gameObject.tag = "Hitted";
+            print("Le renard " + hitVerbs[random.Next(0, 7)] + " " + obj_Collider.gameObject.name);
+            // Change le Tag du GameObject pour ne gagner qu'une fois les points à sa collisionq
+            obj_Collider.gameObject.tag = "Hitted";
             // Retire le static de l'objet pour qu'il puisse de nouveau être bougé par des forces extérieurs
-            obj_rb.isKinematic = false;
-            print(hit.gameObject.name + " : tag = " + hit.gameObject.tag + "    | kinetic = " + obj_rb.isKinematic.ToString());
+            objCol_RB.isKinematic = false;
+            print(obj_Collider.gameObject.name + " : tag = " + objCol_RB.gameObject.tag + "    | kinetic = " + objCol_RB.isKinematic.ToString());
             // Simule une explosion physique type grenade ;   Vector3.down = baisse l'épicentre de l'explosion pour que les objets se propulse plus vers le haut ;   upwardsModifier = ? ;    ForceMode = ?
-            obj_rb.AddExplosionForce(f_explosionForce, transform.position + Vector3.down, 15);
+            objCol_RB.AddExplosionForce(f_explosionForce, transform.position + Vector3.down, 15);
             // Instancier un effet de particule à la collision
-            Instantiate(hitParticles[Random.Range(0, hitParticles.Length)], hit.gameObject.transform.position, Quaternion.identity);
-            //obj_rb = null;
+            Instantiate(hitParticles[Random.Range(0, hitParticles.Length)], obj_Collider.gameObject.transform.position, Quaternion.identity);
+            // Effet Punch sur le score, vector est l'extension / force du punch, puis la durée de l'effet sur la durée
+            iTween.PunchScale(TextScore, new Vector3(2f, 2f, 2f), 0.75f);
+            int newScore = int.Parse(TextScore.GetComponent<TextMeshProUGUI>().text) + (int)objCol_RB.mass;
+            TextScore.GetComponent<TextMeshProUGUI>().text = newScore.ToString();
+            ScoreAdd.GetComponent<TextMesh>().text = "+" + objCol_RB.mass.ToString();
+            iTween.PunchScale(ScoreAdd, new Vector3(4f, 4f, 4f), 0.75f);
+            Invoke("ResetScoreAdd", 0.75f);
         }
+    }
+
+    public void ResetScoreAdd()
+    {
+        ScoreAdd.GetComponent<TextMesh>().text = "";
     }
 }
